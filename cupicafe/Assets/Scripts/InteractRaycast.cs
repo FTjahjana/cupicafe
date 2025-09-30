07b566ef4800155f0921665d75c2b7ef4afb30ba
FTjahjana <tjahjanafrances@gmail.com>
2025-09-30 16:07:02 +0100

    Class session 1

---
+++ cupicafe/Assets/Scripts/InteractRaycast.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InteractRaycast : MonoBehaviour
+{
+    public PlayerInput playerInput;
+    InputAction interactAction;
+
+    private void Awake()
+    {
+        interactAction = playerInput.actions.FindAction("Interact");
+    }
+
+    // Update is called once per frame
+    void Update()
+    { 
+        //get forward vector
+        Vector3 fwd = transform.TransformDirection(Vector3.forward);
+
+        Ray interactRay = new Ray(transform.position, fwd);
+        RaycastHit hitData;
+
+        Debug.DrawRay(interactRay.origin, interactRay.direction* 10);
+
+        //if our raycast hits something, continue
+        if (Physics.Raycast(interactRay, out hitData))
+        {
+            //checks what kind of thing we hit
+            if (hitData.collider.gameObject.layer == 6)
+            {
+                if (interactAction.triggered)
+                {
+                    print("CLICKED!");
+                }
+            }
+        }
+    }
+}
---
+++ cupicafe/Assets/Scripts/PlayerMovement3D.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement3D : MonoBehaviour
+{
+
+    public PlayerInput playerInput;
+    InputAction moveAction;
+    public CharacterController characterController;
+    public float speedMultiplier = 3;
+
+    InputAction lookAction;
+    public GameObject playerCamera;
+    public float lookSensitivity = 2.0f;
+    public float upDownRange = 80.0f;
+    private float yRotation;
+
+    //public Vector2 moveInput;
+    //public Vector3 horizontalMovement;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        //playerInput = GetComponent<PlayerInput>();
+        moveAction = playerInput.actions.FindAction("Move");
+        lookAction = playerInput.actions.FindAction("Look");
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void OnEnable()
+    {
+        moveAction.Enable();
+        lookAction.Enable();
+    }
+
+    private void OnDisable()
+    {
+        moveAction.Disable();
+        lookAction.Disable();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log(moveAction.ReadValue<Vector2>());
+
+       Move();
+       Look();
+    }
+    
+    
+    void Move()
+    {
+        //take in our WASD or Left Stick values
+        Vector2 moveInput = moveAction.ReadValue<Vector2>() * speedMultiplier;
+
+        //convert this to a 3D vector 
+        Vector3 horizontalMovement = new Vector3(moveInput.x, 0, moveInput.y);
+
+        //accounts for rotation
+        horizontalMovement = transform.rotation * horizontalMovement;
+
+        //move the character
+        characterController.SimpleMove(horizontalMovement);
+    }
+
+    void Look()
+    {
+        Vector2 lookInput = lookAction.ReadValue<Vector2>();
+
+        float xRotation = lookInput.x * lookSensitivity * Time.deltaTime * 100;
+        transform.Rotate(0, xRotation, 0);
+
+        yRotation -= lookInput.y * lookSensitivity * Time.deltaTime * 100;
+        yRotation = Mathf.Clamp(yRotation, -upDownRange, upDownRange);
+        playerCamera.transform.localRotation = Quaternion.Euler(yRotation, 0, 0);
+    } 
+}
